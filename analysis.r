2d1c7fe1d88381c8605e933ebc658e79755c7276
Emma Paris <98559241+vjeyam@users.noreply.github.com>
2023-12-22 00:30:12 -0600

    added rand forest reg code block

---
+++ analysis.r
@@ -0,0 +1,13 @@
+# #-------------- Import libraries and packages --------------#
+# library(tidyverse)
+# library(dplyr)
+# library(ggplot2)
+
+# #-------------- Import datasets --------------#
+# nba_all_stars <- read_csv("../input/nba-all-star-players-and-stats-1980-2022/final_data.csv")
+
+# #-------------- Clean data / Feature engineering --------------#
+
+# #-------------- Split data into features / Target / Train / Validation --------------#
+
+# #-------------- Trains a random forest classifier model --------------#
