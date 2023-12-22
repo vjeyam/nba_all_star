56804a897cd273e3c25e04ee897cc52886d8a9d0
Emma Paris <98559241+vjeyam@users.noreply.github.com>
2023-12-21 22:37:12 -0600

    skeleton of analysis file

---
+++ analysis.py
@@ -0,0 +1,11 @@
+#-------------- Import libraries and packages --------------#
+
+#-------------- Import datasets --------------#
+
+#-------------- Clean data / Feature engineering --------------#
+
+#-------------- Split data into features / Target / Train / Validation --------------#
+
+# Different Model types
+
+#--------------  --------------#
