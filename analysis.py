6734c185b378a9a76f0106342938910be42c2c31
Vishal Jeyam <98559241+vjeyam@users.noreply.github.com>
2023-12-24 00:59:32 -0600

    3 point evolution (#1)
    * added rand forest reg code block
    
    * updated analysis.py and .r files
    
    * updating call function for three_point_evol()

--- analysis.py
+++ analysis.py
@@ -1,11 +1,44 @@
 #-------------- Import libraries and packages --------------#
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #-------------- Import datasets --------------#
+nba_all_stars = pd.read_csv("../input/nba-all-star-players-and-stats-1980-2022/final_data.csv")
 
 #-------------- Clean data / Feature engineering --------------#
 
 #-------------- Split data into features / Target / Train / Validation --------------#
 
-# Different Model types
+#-------------- Trains a random forest classifier model --------------#
+def three_point_evolution() -> None:
+    
+    # Assuming 'year' and 'fg3a' are column in your DataFrame
+    x = nba_all_stars[['year']]
+    y = nba_all_stars[['fg3a']]
+    
+    # Split the data into training and testing sets
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    
+    # Use scikit-learn for some machine learning task (ie. regression)
+    model = RandomForestRegressor()
+    model.fit(x_train, y_train)
+    
+    # Make predictions
+    predictions = model.predict(x_test)
+    
+    # Plot the actus vs predicted values
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x=x_test['year'], y=y_test, color='blue', label='Actual')
+    sns.scatterplot(x=x_test['year'], y=predictions, color='red', label='Predicted')
+    plt.xlabel('Year')
+    plt.ylabel(('3-Pointers Attempted'))
+    plt.title('Actual vs Predicted 3-Pointers Attempted by NBA All Stars')
+    plt.legend()
+    plt.show()
+#--------------------------------------------------------------------#
 
-#--------------  --------------#
+# Call models
+three_point_evolution()
---
+++ analysis.r
@@ -0,0 +1,31 @@
+# #-------------- Import libraries and packages --------------#
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+#-------------- Import datasets --------------#
+nba_all_stars <- read_csv("../input/nba-all-star-players-and-stats-1980-2022/final_data.csv")
+
+#-------------- Clean data / Feature engineering --------------#
+
+#-------------- Split data into features / Target / Train / Validation --------------#
+
+#-------------- See how the number of three pointers changed over time --------------#
+three_point_evolution <- function(nba_all_stars) {
+    # Group the all stars by year and find average number of 3 pointers attempted by all stars
+    nba_all_stars %>%
+        group_by(year) %>%
+        summarize(avg_fg3a = mean(fg3a, na.rm = TRUE)) -> avg_3_attempt
+
+    # plot the average number of three pointers attempted by all stars over the years of the data
+    avg_3_attempt %>%
+        ggplot(mapping = aes(x = year, y = avg_fg3a)) +
+        geom_point(color = "blue") + 
+        geo_smooth(color = "black", alpha = 0.25) + 
+        geo_vline(xintercept = 2009, color = "gold") # adds a vertical line at 2009
+}
+#--------------------------------------------------------------------#
+
+# Call functions
+result_plot <- three_point_evolution(your_nba_all_stars_data_frame) # update 'your_nba_all_stars_data_frame' with a data frame
+print(result_plot)  # Optional if you want to display the plot
